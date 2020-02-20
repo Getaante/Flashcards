48499794e34090f934b5f66fa3fdc8e86d3e29d2
Getaante Yilma <getaante.yilma@udc.edu>
2020-02-20 18:56:55 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by m on 2/20/20.
+//  Copyright © 2020 GY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
